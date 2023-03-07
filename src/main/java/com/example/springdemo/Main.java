053e2fd2e724678be9d77b73ae559b74a6b576b8
Promise <promiseprince01@gmail.com>
2023-03-07 16:47:19 +0100

    Opening commit

---
+++ src/main/java/com/example/springdemo/Main.java
@@ -0,0 +1,13 @@
+package com.example.springdemo;
+
+public class Main {
+    String x = "java: as java is working";
+    String y = "weekend: you should be resting";
+
+    public static void main(String[] args) {
+        Main myObj = new Main();
+        System.out.println(myObj.x);
+        System.out.println(myObj.y);
+    }
+
+}
