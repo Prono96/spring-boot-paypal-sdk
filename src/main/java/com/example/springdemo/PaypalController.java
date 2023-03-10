55596af3975a61980c54b78b1e67668a956c7baa
Promise <promiseprince01@gmail.com>
2023-03-10 23:16:16 +0100

    added try and catch block in paypal controller

--- src/main/java/com/example/springdemo/PaypalController.java
+++ src/main/java/com/example/springdemo/PaypalController.java
@@ -1,5 +1,8 @@
 package com.example.springdemo;
 
+import com.paypal.api.payments.Links;
+import com.paypal.api.payments.Payment;
+import com.paypal.base.rest.PayPalRESTException;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -9,12 +12,29 @@ import org.springframework.web.bind.annotation.ModelAttribute;
 public class PaypalController {
     @Autowired
     PaypalService service;
+    public static final String SUCCESS_URL = "pay/success";
+    public static final String CANCEL_URL = "pay/cancel";
+
     @GetMapping("/")
     public String home() {
         return "home";
     }
 
-//    public String payment(@ModelAttribute("order") Order order) {
-//        service.createPayment()
-//    }
+    public String payment(@ModelAttribute("order") Order order) {
+        try {
+            Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
+                    order.getIntent(), order.getDescription(), "http://localhost:9090/" + CANCEL_URL,
+                    "http://localhost:9090/" + SUCCESS_URL);
+            for(Links link:payment.getLinks()) {
+                if(link.getRel().equals("approval_url")) {
+                    return "redirect:"+link.getHref();
+                }
+            }
+
+        } catch (PayPalRESTException e) {
+
+            e.printStackTrace();
+        }
+        return "redirect:/";
+    }
 }
