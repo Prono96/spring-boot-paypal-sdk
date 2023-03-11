cc10d33a5b090e81e75658591c68e5ee1bf42ab2
Promise <promiseprince01@gmail.com>
2023-03-11 16:02:48 +0100

    Added success and failure html page and also more configuration to paypal controller

--- src/main/java/com/example/springdemo/PaypalController.java
+++ src/main/java/com/example/springdemo/PaypalController.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class PaypalController {
@@ -15,16 +17,17 @@ public class PaypalController {
     public static final String SUCCESS_URL = "pay/success";
     public static final String CANCEL_URL = "pay/cancel";
 
-    @GetMapping("/")
+    @GetMapping("/pay")
     public String home() {
         return "home";
     }
 
+    @PostMapping("/pay")
     public String payment(@ModelAttribute("order") Order order) {
         try {
             Payment payment = service.createPayment(order.getPrice(), order.getCurrency(), order.getMethod(),
-                    order.getIntent(), order.getDescription(), "http://localhost:9090/" + CANCEL_URL,
-                    "http://localhost:9090/" + SUCCESS_URL);
+                    order.getIntent(), order.getDescription(), "http://localhost:7000/" + CANCEL_URL,
+                    "http://localhost:7000/" + SUCCESS_URL);
             for(Links link:payment.getLinks()) {
                 if(link.getRel().equals("approval_url")) {
                     return "redirect:"+link.getHref();
@@ -37,4 +40,24 @@ public class PaypalController {
         }
         return "redirect:/";
     }
+
+    @GetMapping(value = CANCEL_URL)
+    public String cancelPay() {
+        return "cancel";
+    }
+
+    @GetMapping(value = SUCCESS_URL)
+    public String successPay(@RequestParam("paymentId") String paymentId, @RequestParam("PayerID") String payerId) {
+        try {
+            Payment payment = service.executePayment(paymentId, payerId);
+            System.out.println(payment.toJSON());
+            if (payment.getState().equals("approved")) {
+                return "success";
+            }
+        } catch (PayPalRESTException e) {
+            System.out.println(e.getMessage());
+        }
+        return "redirect:/";
+    }
+
 }
